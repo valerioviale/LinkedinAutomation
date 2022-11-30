e7990e704714ce0054c17c16dca650d1c87c28ca
valerioviale <34212301+valerioviale@users.noreply.github.com>
2022-11-30 16:04:33 +0100

    Update LinkedinSeleniumAutoLogin.py
    Added for cycle to replicate the action to send a message

--- LinkedinSeleniumAutoLogin.py
+++ LinkedinSeleniumAutoLogin.py
@@ -31,23 +31,25 @@ all_buttons = driver.find_elements(By.TAG_NAME,"buttons")
 message_buttons =  [btn for btn in all_buttons if "Message" in btn.text]
 time.sleep(2)
 
-message_buttons[0].click()
-time.sleep(2)
 
-main_div = driver.find_element(By.XPATH,"//div[starts-with(@class, 'msg-form__msg-content-container')]")
-main_div.click()
-paragraphs = driver.find_elements(By.TAG_NAME,"p")
-paragraphs[-5].send_keys("automated test message")
+for i in range(0, len(message_buttons)):
 
-time.sleep(4)
+    message_buttons[i].click()
+    time.sleep(2)
+    main_div = driver.find_element(By.XPATH,"//div[starts-with(@class, 'msg-form__msg-content-container')]")
+    main_div.click()
+    paragraphs = driver.find_elements(By.TAG_NAME,"p")
+    paragraphs[-5].send_keys("This is a test message for linkedin automation")
 
-submit = driver.find_element(By.XPATH,"//button[@type='submit']").click()
+    time.sleep(4)
 
-time.sleep(2)
+    submit = driver.find_element(By.XPATH,"//button[@type='submit']").click()
+
+    time.sleep(2)
 
 
-close_button = driver.find_element(By.XPATH,"//button[@class = 'msg-overlay-bubble-header__control artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--1 artdeco-button--tertiary ember-view']")
-close_button.click()
+    close_button = driver.find_element(By.XPATH,"//button[@class = 'msg-overlay-bubble-header__control artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--1 artdeco-button--tertiary ember-view']")
+    close_button.click()
 
-time.sleep(4)
+    time.sleep(4)
 
