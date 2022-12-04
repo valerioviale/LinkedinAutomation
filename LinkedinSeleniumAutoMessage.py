16454689d3d796fac68af357fc5d6dc89ac7c154
valerioviale <34212301+valerioviale@users.noreply.github.com>
2022-12-04 17:33:25 +0100

    Adding to more import to make the new webdriver works
    from selenium.webdriver.chrome.service import Service
    from webdriver_manager.chrome import ChromeDriverManager

--- LinkedinSeleniumAutoMessage.py
+++ LinkedinSeleniumAutoMessage.py
@@ -3,8 +3,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
-
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://linkedin.com")
